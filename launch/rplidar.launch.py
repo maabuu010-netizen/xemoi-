fd68e5a692813c3e11849135494ee9bc5b35469c
Hui <maabuu010@gmail.com>
2025-12-28 00:40:30 +0700

    up

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -1,20 +1,22 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
     return LaunchDescription([
-
         Node(
             package='rplidar_ros',
             executable='rplidar_composition',
             output='screen',
             parameters=[{
+                # ưu tiên by-id cho cố định thiết bị
                 'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+
+                # THÊM DÒNG NÀY (quan trọng nhất)
+                'serial_baudrate': 115200,
+
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
-                'scan_mode': 'Standard'
+                'scan_mode': 'Standard',
             }]
         )
     ])
